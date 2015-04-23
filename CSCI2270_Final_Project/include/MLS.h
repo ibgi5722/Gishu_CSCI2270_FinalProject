39eab530c68acd86b44fcfd875ef0ded42a7de19
Kurt <kubo8381@colorado.edu>
2015-04-23 13:42:59 -0600

    updated MLS header file

--- CSCI2270_Final_Project/include/MLS.h
+++ CSCI2270_Final_Project/include/MLS.h
@@ -1,6 +1,31 @@
 #ifndef MLS_H
 #define MLS_H
+#include <iostream>
+#include <vector>
 
+struct SoccerPlayer
+{
+    std::string nameStr;
+    int ageInt;
+    int jerseyNumberInt;
+    std::string positionStr;
+    int minutesInt;
+    int goalsInt;
+    int assistsInt;
+    int savePercentageInt;
+    int yellowCardsInt;
+    int redCardsInt;
+};
+
+struct SoccerTeam
+{
+    std::string nameStr;
+    int goalsInt;
+    int assistsInt;
+    int yellowCardsInt;
+    int redCardsInt;
+    std::vector<SoccerPlayer> vecPlayers;
+};
 
 class MLS
 {
