eccc9e05cf3f54715cfa0533bd36a793e687daa0
Kurt <kubo8381@colorado.edu>
2015-04-28 04:03:52 -0600

    Added the comments that go above every method

--- CSCI2270_Final_Assignment/src/MLS.cpp
+++ CSCI2270_Final_Assignment/src/MLS.cpp
@@ -15,7 +15,13 @@ MLS::~MLS()
     //dtor
 }
 
-
+/*
+1.bool searchVector(std::string tName);
+2.This method gets passed in a team name, and it loops through the vector of team names until a matching string is found. The function returns a boolean that determines whether or not a team is in the vector.
+3.An example of how to call this method is to set it equal to a boolean: bool isTeamInVector = searchVector(teamName)
+4.The pre-condition for this method requires that the vector of team names must already be created
+5.There are no post-conditions for this method, as it simply returns a boolean
+*/
 bool MLS::searchVector(std::string tName)
 {
     bool foundTeam = false; //initializes a boolean that stores if a team is in the vector to false
@@ -29,6 +35,17 @@ bool MLS::searchVector(std::string tName)
     return foundTeam; //returns the boolean
 }
 
+/*
+1.void insertTeam(std::string tName, std::string pName, int pAge, int pNumber, std::string pPosition, int minutes, int goals, int assists, int savePercent, int yellowCards, int redCards);
+2.This method gets passed in a team name, and all the player stats and details from the file. This method initializes the hash table, as well as adding all the teams to a tree before passing the tree
+on to the rbAddFixup and passing the players into another method to be added to the hash table.
+3.To call this method, first a variable of the type of the class must be created, such as MLS mls;
+mls->insertTeam(strTeamName, strPlayerName, intPlayerAge, floatPlayerNumber, strPosition, intMinutes, intGoals, floatAssists, intSavePercentage, intYellowCards, intRedCards);
+This should be done as each line is read in from the file and the data is separated into its appropriate type.
+4.The pre-condition for this method requires that the searchVector function be called to ensure each team is only added to the vector once.
+5.Once this function is completed, the red black tree will be built (and balanced after other functions are called within this method), and the hash table will be built (and filled out after
+calling separate functions within this function). Also,every team will point to a hash table, and every player will have the information necessary to fill out the struct.
+*/
 void MLS::insertTeam(std::string tName, std::string pName, int pAge, int pNumber, std::string pPosition, int minutes, int goals, int assists, int savePercent, int yellowCards, int redCards)
 {
     bool addedToTree = false;
@@ -92,6 +109,13 @@ void MLS::insertTeam(std::string tName, std::string pName, int pAge, int pNumber
     newTeam = searchTree(tName);
 }
 
+/*
+1.SoccerTeam* searchTree(std::string tName);
+2.This method gets passed in a team name, and it traverses through the tree until it finds a match for the team name in the tree. Once a match is found, the method returns the team
+3.An example of how to call this method is to set it equal to a variable of type SoccerTeam: SoccerTeam *newTeam = searchTree(teamName);
+4.The pre-condition for this method requires that the red black tree of all the team names already exist
+5.There are no post-conditions for this method, as it simply returns a SoccerTeam
+*/
 SoccerTeam* MLS::searchTree(std::string tName)
 {
     SoccerTeam *tempRoot = root; //sets a temporary node to the root
@@ -116,11 +140,25 @@ SoccerTeam* MLS::searchTree(std::string tName)
     return tempRoot; //returns the team once it is found in the tree
 }
 
+/*
+1.void printTeams();
+2.This method is the public print team method that is called from the main. It simply calls the private method on the root.
+3.To call this method, first a variable of the type of the class must be created, such as MLS mls; Then, the function can be called by calling mls->printTeams();
+4.The pre-condition for this method requires that the tree exist so that the root exists and the program isn't trying to print NULL
+5.There are no post-conditions for this method, as it simply calls another method
+*/
 void MLS::printTeams()
 {
     printTeams(root); //calls the private print team method on the root
 }
 
+/*
+1.void printTeams(SoccerTeam *temp);
+2.This method is a private method that recursively calls itself starting at the root of the red black tree and printing all the team names in alphabetical order.
+3.An example of how to call this method is to use a public function by calling: printTeams(root);
+4.The pre-condition for this method requires that the public method printTeams() be called so that this function can be called on the root.
+5.There are no post-conditions for this method, as it simply prints team names from the tree.
+*/
 void MLS::printTeams(SoccerTeam *tempRoot)
 {
     if(tempRoot->leftChild != nil)
@@ -134,6 +172,13 @@ void MLS::printTeams(SoccerTeam *tempRoot)
     }
 }
 
+/*
+1.void rbAddFixup(SoccerTeam * x);
+2.This method gets passed in a team and sets the red black properties for the tree. It also calls for left and right rotations so that the red black tree ends up balanced and follows the rules of red black trees.
+3.This method should be called after a team is added to the tree in a way such as this: rbAddFixup(newTeam);
+4.The pre-condition for this method requires that there is at least one team in the red black tree. For that to happen, the insertTeam method must be called before the rbAddFixup.
+5.Once the rbAddFixup method finished running, the red black tree will be balanced in such a way that it fulfills the standard properties and rules of a red black tree.
+*/
 void MLS::rbAddFixup(SoccerTeam * x)
 {
     x->leftChild = nil;
@@ -190,6 +235,14 @@ void MLS::rbAddFixup(SoccerTeam * x)
     root->isRed = false;
 }
 
+/*
+1.void leftRotate(SoccerTeam * x);
+2.This method gets passed in a team from the red black tree, and manipulates the nodes in the tree in such a way that the tree is rotated to the left, in an attempt to follow the rules of red black trees,
+thus balancing the tree.
+3.This method should just be called inside another method: leftRotate(team);
+4.The pre-condition for this method requires that the rbAddFunction call for a left rotation, and that the red black tree must already exist.
+5.The post-conditions of this method are that the teams in the red black tree will be rotated to the left
+*/
 void MLS::leftRotate(SoccerTeam * x)
 {
     SoccerTeam *y;
@@ -216,6 +269,14 @@ void MLS::leftRotate(SoccerTeam * x)
     x->parent = y;
 }
 
+/*
+1.void rightRotate(SoccerTeam * y);
+2.This method gets passed in a team from the red black tree, and manipulates the nodes in the tree in such a way that the tree is rotated to the right, in an attempt to follow the rules of red black trees,
+thus balancing the tree.
+3.This method should just be called inside another method: rightRotate(team);
+4.The pre-condition for this method requires that the rbAddFunction call for a right rotation, and that the red black tree must already exist.
+5.The post-conditions of this method are that the teams in the red black tree will be rotated to the right
+*/
 void MLS::rightRotate(SoccerTeam * y)
 {
     SoccerTeam *x = y->leftChild;
@@ -241,6 +302,13 @@ void MLS::rightRotate(SoccerTeam * y)
     y->parent = x;
 }
 
+/*
+1.bool searchVector(std::string tName);
+2.This method gets passed in a players name, and calculates the hash sum of that name by looping through the string. The remainder of this sum when divided by 10 is returned.
+3.An example of how to call this method is to set it equal to an integer: int sum = hashSum(playerName);
+4.The pre-condition for this method requires that the insertPlayer function be called, as that is where players are added to the hash table and where the hash sum is needed.
+5.There are no post-conditions for this method, as it simply returns an integer that will be used to determine the placement of a player in the hash table.
+*/
 int MLS::hashSum(std::string playerName)
 {
     int i = 0;
@@ -251,6 +319,14 @@ int MLS::hashSum(std::string playerName)
     return i%10; //returns the remainder when dividing the sum by 10
 }
 
+/*
+1.void insertPlayer(SoccerPlayer *player, SoccerPlayer **hashTable);
+2.This method gets passed in a player of the struct type SoccerPlayer and the hash table that the player will be added to. The player is added to the hash table if the index of that player is NULL, or
+the player is added to a linked list at that index if there is already a player(or players) there.
+3.An example of how to call this method is: insertPlayer(newPlayer, hashTable);
+4.The pre-condition for this method requires that the hash table be initialized to NULL and a pointer from the team to hash table be established
+5.The post-conditions are that once this method has been completed, a new player will have been added to a specific hash table that is pointed to by a team in the red black tree.
+*/
 void MLS::insertPlayer(SoccerPlayer *player, SoccerPlayer **hashTable)
 {
     int i = hashSum(player->nameStr); //finds the hash sum of the players name
@@ -269,6 +345,13 @@ void MLS::insertPlayer(SoccerPlayer *player, SoccerPlayer **hashTable)
     }
 }
 
+/*
+1.SoccerTeam* selectTeam(std::string tName);
+2.This method gets passed in a team name, and it loops through the red black tree of teams until a match is found. Once the team is found, the node is returned.
+3.An example of how to call this method is to set it equal to a variable of type SoccerTeam: SoccerTeam *TeamPointer = mls->selectTeam(team);
+4.The pre-condition for this method requires that the user input a team name and that the red black tree of teams already be built
+5.There are no post-conditions for this method, as it simply returns a node
+*/
 SoccerTeam* MLS::selectTeam(std::string tName)
 {
     SoccerTeam *node = root;
@@ -294,6 +377,13 @@ SoccerTeam* MLS::selectTeam(std::string tName)
     return NULL;
 }
 
+/*
+1.void printRoster(SoccerTeam *temp);
+2.This method gets passed in a team selected by the user and loops through that teams player has table, storing them all in a vector, and passing that vector into the sortRoster method.
+3.To call this method, first a variable of the type of the class must be created, such as MLS mls; Then, the function can be called by calling mls->printRoster(team)
+4.The pre-condition for this method requires that the hash table of players already be built, and that the selectTeam method has already been called and stored a team to pass into the method.
+5.There are no post-conditions for this method, as it simply stores the hash table in a temporary vector which is then passed into a new function to be sorted and printed
+*/
 void MLS::printRoster(SoccerTeam *temp)
 {
     std::vector<SoccerPlayer*> collisions;
@@ -318,6 +408,15 @@ void MLS::printRoster(SoccerTeam *temp)
     }
     sortRoster(collisions); //once the vector is created it will be sorted in the sort roster method
 }
+
+/*
+1.void sortRoster(std::vector<SoccerPlayer*> collisions);
+2.This method gets passed in a vector that contains a teams roster so that it may be sorted. The roster is sorted alphabetically and once the sort is finished, the method loops through
+the vector and prints the player names.
+3.An example of how to call this method is to simply call sortRoster(vectorName); as nothing is returned
+4.The pre-condition for this method requires that the hash table of players already be stored in a temporary vector, and that the printRoster function be called, as that is where the sortRoster is called from.
+5.There are no post-conditions for this method, as it stores the roster in a temporary vector and prints the sorted data
+*/
 void MLS::sortRoster(std::vector<SoccerPlayer*> collisions)
 {
     bool newBool = true;
@@ -345,6 +444,13 @@ void MLS::sortRoster(std::vector<SoccerPlayer*> collisions)
     }
 }
 
+/*
+1.void printPlayerInfo(std::string player, SoccerTeam *team);
+2.This method gets passed in a team  pointer and a players name. The method loops through the teams roster to find the player specified by the user, and the players basic info and statistics are printed.
+3.To call this method, first a variable of the type of the class must be created, such as MLS mls; Then, the function can be called by calling mls->printPlayerInfo(playerName, team)
+4.The pre-condition for this method requires that the hash table of a teams roster be created already, and that the selectTeam method has already been called and that team is being passed into the method
+5.There are no post-conditions for this method, as it is just printing a players data and not affecting the pointers
+*/
 void MLS::printPlayerInfo(std::string player, SoccerTeam *team)
 {
     bool boolfoundPlayer = false;
@@ -406,6 +512,14 @@ void MLS::printPlayerInfo(std::string player, SoccerTeam *team)
     }
 }
 
+/*
+1.void sortPlayers(int menuOption, SoccerTeam *team);
+2.This method gets passed in an integer called menuOption and a team. The integer will be read in from the terminal based on the statistic that the user wants to sort the roster by. There are 7
+if statements and they each run the same sort, but use different sets of integers based on what the user selects.
+3.To call this method, first a variable of the type of the class must be created, such as MLS mls; Then, the function can be called by calling mls->(menuOption, team)
+4.The pre-condition for this method requires that the hash table of players already be created, and that the selectTeam method has already been called, and that is what is being passed into the method
+5.There are no post-conditions for this method, as it simply prints sorted data that is only stored in temporary variables
+*/
 void MLS::sortPlayers(int menuOption, SoccerTeam *team)
 {
     std::vector<SoccerPlayer*> playersVector;
@@ -622,6 +736,15 @@ void MLS::sortPlayers(int menuOption, SoccerTeam *team)
 
 }
 
+
+/*
+1.void printTeamStats(SoccerTeam *team);
+2.This method gets passed in a team of type SoccerTeam based on user input from the terminal. The function calculates the sum of the various stats of the players on a teams roster,
+as well as average minutes and average age, which is all printed to the terminal.
+3.To call this method, first a variable of the type of the class must be created, such as MLS mls; Then, the function can be called by calling mls->printTeamStats(team)
+4.The pre-condition for this method requires that the hash table of players already be built, and that the selectTeam method has already been called and stored a team to pass into the method.
+5.There are no post-conditions for this method, as it is only printing data
+*/
 void MLS::printTeamStats(SoccerTeam *team)
 {
     int j = 0;
